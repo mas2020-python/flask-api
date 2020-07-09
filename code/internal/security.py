928735c62e01aaeda8ea69c3f025b86095506f64
Andrea Genovesi <andrea.genovesi@appway.com>
2020-07-09 09:28:30 +0200

    add safe_str_cmp to compare two strings

--- code/internal/security.py
+++ code/internal/security.py
@@ -5,11 +5,12 @@ We have to mandatory methods:
 - identity: use to get the payload
 """
 import tmpdb
+from werkzeug.security import safe_str_cmp
 
 
 def authenticate(username, password):
     user = tmpdb.User.find_by_username(username)
-    if user and user.password == password:
+    if user and safe_str_cmp(user.password, password):
         return user
 
 
