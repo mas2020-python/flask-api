8d40eb1b8c7f37d742ad8268d4b4cc3994dfea95
Andrea Genovesi <andrea.genovesi@appway.com>
2020-07-28 12:50:39 +0200

    add logging with configuration file

--- code/app.py
+++ code/app.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from datetime import timedelta
 from flask import Flask
@@ -9,11 +10,15 @@ from internal.security import authenticate, identity
 from internal.db import db
 from utils.config import API_SRV
 import toml
+import logging
+import logging.config
+from flask.logging import default_handler
 
 """
 Using flask_restful jsonify for object that are not dictionary is not needed cause flask restful
 do it for us.
 """
+logger: logging.Logger
 
 def read_api_config():
     try:
@@ -21,6 +26,22 @@ def read_api_config():
     except Exception as e:
         print("Houston, we have a problem: ", e.__str__())
         sys.exit(1)
+
+
+# Set the logger
+def set_logger():
+    global logger
+    try:
+        # Does log file exist?
+        if not os.path.isfile(API_SRV.config['log']['conf_file']):
+            raise Exception("log config file ({}) not found!".format(API_SRV.config['log']['conf_file']))
+        logging.config.fileConfig(fname=API_SRV.config['log']['conf_file'], disable_existing_loggers=True)
+        # Get the logger specified in the file
+        logger = logging.getLogger(API_SRV.config['log']['default_logger'])
+    except Exception as e:
+        print("Houston, we have a problem: ", e.__str__())
+        sys.exit(1)
+
 
 def create_app() -> (Flask, Api):
     # creating main app
@@ -52,11 +73,27 @@ def add_resources(api: Api):
 
 # start point for the Application
 if __name__ == "__main__":
-    # read config
-    read_api_config()
-    app, api = create_app()
-    add_resources(api)
-    # connect the SQLAlchemy object to the app
-    db.init_app(app)
-    # run the test server, Debug=True helps to debug in case of any error
-    app.run(port=API_SRV.config['server']['port'], debug=True)
+    try:
+        # read config and load logger
+        read_api_config()
+        app, api = create_app()
+        set_logger()
+        # log = logging.getLogger('werkzeug')
+        # log.disabled = True
+        # print(logger.handlers)
+        add_resources(api)
+        # connect the SQLAlchemy object to the app
+        db.init_app(app)
+        # run the test server, Debug=True helps to debug in case of any error
+        api_env = os.environ[API_SRV.config['server']['api_env']]
+        if api_env == 'test':
+            logger.info(f"Application is starting in TEST environment (version: {API_SRV.config['server']['version']})")
+            app.run(port=API_SRV.config['server']['port'], debug=True)
+        else:
+            logger.info(f"Application is starting in PRODUCTION env (version: {API_SRV.config['server']['version']})")
+    except KeyError as e:
+        logger.error(f"Houston, we have a problem finding the env variable: {str(e)}")
+        sys.exit(1)
+    except Exception as e:
+        logger.error(f"Houston, we have a problem: {str(e)}")
+        sys.exit(1)
